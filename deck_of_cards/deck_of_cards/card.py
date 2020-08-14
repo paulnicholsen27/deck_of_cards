6f5f1c5723c70a1bbfa0fcadf75827f1eb478fda
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-08-14 17:55:08 -0400

    created Card class

--- deck_of_cards/deck_of_cards/card.py
+++ deck_of_cards/deck_of_cards/card.py
@@ -1,2 +1,14 @@
 class Card:
-    pass
+    
+    def __init__(self, color, rank):
+        if color not in ["red", "yellow", "green"]:
+            raise ValueError
+        self.color = color 
+        if rank not in range(0, 9):
+            raise ValueError
+        self.rank = rank 
+    
+    def score(self):
+        color_values = {"red": 3, "yellow": 2, "green":1}
+        return color_values[self.color] * self.rank 
+    
