7670ecb743d841e3d6282b71135af68cf8055fc6
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-08-14 17:08:30 -0400

    added card tests

---
+++ deck_of_cards/tests/test_card.py
@@ -0,0 +1,23 @@
+import pytest
+from deck_of_cards.card import Card 
+
+def test_card_properties():
+    card = Card("red", 7)
+    assert(card.color == "red")
+    assert(card.rank == 7)
+
+def test_card_score():
+    green_card = Card("green", 5)
+    yellow_card = Card("yellow", 4)
+    red_card = Card("red", 8)
+
+    assert(green_card.score == 5) # 1 * 5
+    assert(yellow_card.score == 8) # 2 * 4
+    assert(red_card.score == 24) # 3 * 8
+
+def test_valid_input():
+    with pytest.raises(ValueError):
+        Card("purple", 7)
+
+    with pytest.raises(ValueError):
+        Card("red", 17)
